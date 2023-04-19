ce22c887a003fbab4bc010d50c51362e908ed40f
santiariasMeli <santiago.ariasgarcia@mercadolibre.com.co>
2023-04-19 13:34:46 -0500

    arquitectura hexagonal

--- superhero/src/main/java/com/meli/superhero/repository/SuperHeroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meli.superhero.repository;
-
-import com.meli.superhero.model.SuperHero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuperHeroRepository extends JpaRepository<SuperHero, Integer> {
-}
